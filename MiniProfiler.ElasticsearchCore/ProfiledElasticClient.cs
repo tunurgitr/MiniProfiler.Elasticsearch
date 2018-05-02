f7676c4a3b2d2c3c03d079697e8a8c851243e528
Jeff Harris <jharrisaz@gmail.com>
2018-05-02 12:01:14 -0700

    initial migration

---
+++ MiniProfiler.ElasticsearchCore/ProfiledElasticClient.cs
@@ -0,0 +1,25 @@
+﻿namespace StackExchange.Profiling.Elasticsearch
+{
+    using Nest;
+    using Utils;
+
+    /// <summary>
+    /// Profiled version of <see cref="ElasticClient"/>. Handles responses and pushes data to current <see cref="MiniProfiler"/>'s session.
+    /// </summary>
+    public class ProfiledElasticClient : ElasticClient
+    {
+        private readonly MiniProfiler _profiler = MiniProfiler.Current;
+
+        /// <summary>
+        /// Provides base <see cref="ElasticClient"/> with profiling features to current <see cref="MiniProfiler"/> session.
+        /// </summary>
+        /// <param name="configuration">Instance of <see cref="ConnectionSettings"/>. Its responses will be handled and pushed to <see cref="MiniProfiler"/></param>
+        public ProfiledElasticClient(ConnectionSettings configuration)
+            : base(configuration)
+        {
+            ProfilerUtils.ExcludeElasticsearchAssemblies();
+            ProfilerUtils.ApplyConfigurationSettings(configuration);
+            configuration.OnRequestCompleted(apiCallDetails => MiniProfilerElasticsearchCore.HandleResponse(apiCallDetails, _profiler));
+        }
+    }
+}
